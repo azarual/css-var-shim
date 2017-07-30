120b1518a1646d8254f0a6dd46ed14150c97cc0f
Wesley Luyten <hi@wesleyluyten.com>
2017-07-30 18:15:28 -0400

    Improve shim to support setting of the defining CSS vars

--- src/js/index.js
+++ src/js/index.js
@@ -1,61 +1,100 @@
 
+window.cssVarCache = {
+  setVars: {}
+};
+
 function onCssVarMap(cssVarMap) {
 
-  window.cssVarCache = [];
-  window.cssVarSupport = window.CSS && CSS.supports && CSS.supports('--a', 0);
-  //cssVarSupport = false;
+  var originalSetProperty = CSSStyleDeclaration.prototype.setProperty;
+  CSSStyleDeclaration.prototype.setProperty = function (prop, value, priority) {
+    if (/^--/.test(prop)) {
+      var cacheMap = window.cssVarCache.setVars;
+      cacheMap[prop] = value;
 
-  if (!cssVarSupport) {
+      var count = makeCount();
+      var cssRules = getCssRules(document.styleSheets);
+      cssRules.forEach(function (rule) {
+        var selector = rule.selectorText;
+        var varDecls = objectResolve(cssVarMap.getVars, [prop, selector, count(selector)]);
+        if (varDecls) {
+          varDecls.forEach(niceArguments(function (mapProp, mapValue, mapPriority) {
+            var replacedValue = replaceVarsInValue(mapValue, cacheMap);
+            // IE doesn't like undefined as the important argument
+            rule.style.setProperty(mapProp, replacedValue, mapPriority || null);
+          }));
+        }
+      });
+      return;
+    }
+    originalSetProperty.call(this, prop, value, priority);
+  };
 
-    var originalSetProperty = CSSStyleDeclaration.prototype.setProperty;
-    CSSStyleDeclaration.prototype.setProperty = function(prop, value, priority) {
-      if (/^--/.test(prop)) {
-        var varRegex = new RegExp('var\\(' + prop + '(,[^)]+)?\\)');
-        var cssRules = getCssRules(document.styleSheets);
-        cssRules.forEach(function(rule) {
-          var selector = rule.selectorText;
-          var varDecls = objectResolve(cssVarMap.getVars, [ prop, selector, count(selector) ]);
-          if (varDecls) {
-            varDecls.forEach(niceArguments(function(mapProp, mapValue, mapPriority) {
-              var replacedValue = mapValue.replace(varRegex, value);
-              // IE doesn't like undefined as the important argument
-              rule.style.setProperty(mapProp, replacedValue, mapPriority || null);
-            }));
-          }
-        });
-        return;
-      }
-      originalSetProperty.call(this, prop, value, priority);
-    };
+  function replaceVarsInValue(value, map) {
+    var matchGetVar = /--[^\s,)]+/g;
+    var getVarMatches = getMatches(value, matchGetVar);
+    if (getVarMatches.length) {
+      getVarMatches.forEach(function (getVarMatch) {
+        var getVar = getVarMatch[0];
+        var getVarValue = objectResolve(map, [getVar]);
+        if (getVarValue) {
+          var varRegex = new RegExp('var\\(' + getVar + '(,[^)]+)?\\)');
+          value = value.replace(varRegex, getVarValue);
+          value = replaceVarsInValue(value, map);
+        }
+      });
+    }
+    return value;
+  }
+
+  function setVar(prop, value, important, selector, selectorIndex) {
+    var elements = [ document.documentElement ];
+    if (!selector || selector !== ':root') {
+      elements = document.querySelectorAll(selector);
+    }
+    elements.forEach(function (element) {
+      element.style.setProperty(prop, value, important || null);
+    });
   }
 
   function getCssRules(cssRulesObject) {
-    return arrayFrom(cssRulesObject).reduce(function(prev, curr) {
+    return arrayFrom(cssRulesObject).reduce(function (prev, curr) {
       return prev.concat(curr.cssRules ? getCssRules(curr.cssRules) : curr);
     }, []);
   }
 
+  function getMatches(str, regex, result) {
+    result = result || [];
+    var match;
+    while ((match = regex.exec(str)) !== null) {
+      result.push(match);
+      regex.lastIndex = match.index + match[0].length;
+    }
+    return result;
+  }
+
   function niceArguments(fn) {
-    return function(decl) {
+    return function (decl) {
       fn.apply(this, decl);
     }
   }
 
   function objectResolve(obj, props) {
-    return props.reduce(function(prev, curr) {
+    return props.reduce(function (prev, curr) {
       return prev ? prev[curr] : undefined
     }, obj);
   }
 
-  var count = (function makeCount(countMap) {
+  function makeCount(countMap) {
       countMap = countMap || {};
       return function (key) {
           countMap[key] = isNaN(countMap[key]) ? 0 : countMap[key] + 1;
           return countMap[key];
       };
-  }());
+  };
 
   function arrayFrom(object) {
     return [].slice.call(object);
   }
+
+  cssVarMap.setVars.forEach(niceArguments(setVar));
 }
